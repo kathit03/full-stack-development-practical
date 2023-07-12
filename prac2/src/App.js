48feaaa39302ecf4d5cfe6677a933047152abdb2
kathit03 <kathitshah007@gmail.com>
2023-07-12 10:34:51 +0530

    hello

---
+++ prac2/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code>HELLO WORLD APPLICATION</code> 
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/kathit03/full-stack-development-practical"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          kathit shah
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
